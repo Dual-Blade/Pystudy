2aa069a98d74fd67d7ba38fdb265c5fa7b395929
Dual-Blade <xysdual@163.com>
2020-12-31 17:35:57 +0800

    daily study

--- AI.py
+++ AI.py
@@ -3,6 +3,8 @@ import pandas as pd
 import os
 import tarfile
 import urllib
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -11,7 +13,7 @@ HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     """
-    docstring
+    Get data from housing_url and download in housing_path.
     """
     os.makedirs(housing_path, exist_ok=True)
     tgz_path = os.path.join(housing_path, "housing.tgz")
@@ -23,14 +25,38 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 
 def load_housing_data(housing_path=HOUSING_PATH):
     """
-    docstring
+    Load csv from local housing_path and return a DataFrame.
     """
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
 
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+"""Download and load data"""
 # fetch_housing_data()
 housing = load_housing_data()
+# %matplotlib inline
+# housing.hist(bins=50, figsize=(20, 15))
 
 # %%
-housing.info()
+"""Split train set and test set"""
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+housing["income_cat"].hist()
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    start_train_set = housing.loc[train_index]
+    start_test_set = housing.loc[test_index]
+
+for set_ in (start_train_set, start_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+# %%
